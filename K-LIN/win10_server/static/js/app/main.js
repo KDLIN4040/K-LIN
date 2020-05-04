f1ba8c6a8705b3f87ab7e3501c778b2ecd38ffd4
bbtan <44172194+KDLIN4040@users.noreply.github.com>
2020-05-04 15:45:34 +0800

    ip change

--- K-LIN/win10_server/static/js/app/main.js
+++ K-LIN/win10_server/static/js/app/main.js
@@ -147,7 +147,7 @@ define( function( require ) {
 
   function imu(){
     $.ajax({
-    url:'http://192.168.43.189:81/getattitude',
+    url:'http://192.168.43.190:81/getattitude',
     type:'get',
     cache:false,
     success:function(response){
